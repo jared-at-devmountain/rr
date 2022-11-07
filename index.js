51632a4ad3febbb3564affe735b26d5716f9cef6
Jared Collier <jared@Jareds-MBP.localdomain>
2022-11-07 11:39:23 -0700

    rotating image, looping vid

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-const bigButton = document.querySelector('#meanOldDroid')
+const im = document.getElementById('im')
+const vid = document.getElementById('vid')
 
-const message = () => {
-    alert('General Kenobi!')
+const imClick = () => {
+    for(var i=1;i<11;i++) {
+        setInterval((function(myI) {
+            return function()
+            {
+                im.style.transitionDuration='2s';
+                im.style.transform = 'rotate(-90deg)';
+            };
+        }(i)),100);
+    }
+
+    setInterval((function(myI) {
+        return function() {
+            im.style.display='none';
+            vid.style.display='block';
+        }
+    }(i)), 3000)
 }
 
-bigButton.addEventListener('click', message)
+im.addEventListener('click', imClick)
