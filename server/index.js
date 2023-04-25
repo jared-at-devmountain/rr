e4e8835e502dcdfbfe41d56f0137fed9b1383422
Jared Collier <jared@Jareds-MBP.localdomain>
2023-04-25 14:31:23 -0600

    changes port number

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.get('/js', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.js'))
 })
 
-const port = process.env.PORT || 4005
+const port = process.env.PORT || 4000
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
